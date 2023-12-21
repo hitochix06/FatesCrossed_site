42e914540e9d4029960f294db048e41d75ecfd0f
hitochix06 <pachara.philibert@hotmail.fr>
2023-12-21 21:12:21 +0100

    Update Navigation.vue component labels

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -47,25 +47,13 @@
           as="a"
           href="#"
           class="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
-          >Dashboard</DisclosureButton
+          >Accueil</DisclosureButton
         >
         <DisclosureButton
           as="a"
           href="#"
           class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >Team</DisclosureButton
-        >
-        <DisclosureButton
-          as="a"
-          href="#"
-          class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >Projects</DisclosureButton
-        >
-        <DisclosureButton
-          as="a"
-          href="#"
-          class="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
-          >Calendar</DisclosureButton
+          >À propos</DisclosureButton
         >
       </div>
     </DisclosurePanel>
