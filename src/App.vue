a7736243e5cf1b1fb471b4fd45a9bcda2440bea7
hitochix06 <pachara.philibert@hotmail.fr>
2023-12-25 17:41:33 +0100

    Fix import case in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import Navigation from "./components/navigation.vue";
+import Navigation from "./components/Navigation.vue";
+import Accueil from "./components/Accueil.vue";
 </script>
 
 <template>
   <div>
     <Navigation />
+    <Accueil />
   </div>
 </template>
 
---
+++ src/components/Accueil.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="min-h-screen bg-gray-900 px-4 py-20">
+    <div class="max-w-7xl mx-auto">
+      <div class="text-center">
+        <h1
+          class="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-teal-300"
+        >
+          Visual Novel X
+        </h1>
+        <p class="mt-4 text-xl leading-relaxed text-blue-200">
+          Plongez dans une aventure futuriste où la réalité et les mirages se
+          confondent.
+        </p>
+      </div>
+
+      <div class="mt-20 grid grid-cols-1 md:grid-cols-3 gap-10">
+        <div class="flex flex-col items-center">
+          <div
+            class="w-24 h-24 mb-4 rounded-full bg-gradient-to-r from-blue-500 to-teal-500 flex items-center justify-center"
+          >
+            <span class="text-2xl font-bold text-white">01</span>
+          </div>
+          <h3 class="text-lg font-semibold text-teal-200">
+            Narration Interactive
+          </h3>
+          <p class="mt-2 text-center text-blue-300">
+            Vos choix façonnent l'histoire et déterminent votre destin dans cet
+            univers complexe.
+          </p>
+        </div>
+        <div class="flex flex-col items-center">
+          <div
+            class="w-24 h-24 mb-4 rounded-full bg-gradient-to-r from-green-500 to-teal-500 flex items-center justify-center"
+          >
+            <span class="text-2xl font-bold text-white">02</span>
+          </div>
+          <h3 class="text-lg font-semibold text-teal-200">
+            Graphismes Avancés
+          </h3>
+          <p class="mt-2 text-center text-blue-300">
+            Découvrez des visuels époustouflants alliant style futuriste et
+            esthétique moderne.
+          </p>
+        </div>
+        <div class="flex flex-col items-center">
+          <div
+            class="w-24 h-24 mb-4 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 flex items-center justify-center"
+          >
+            <span class="text-2xl font-bold text-white">03</span>
+          </div>
+          <h3 class="text-lg font-semibold text-teal-200">Son Immersif</h3>
+          <p class="mt-2 text-center text-blue-300">
+            Une bande-son soignée et des effets sonores qui vous plongent au
+            cœur de l'action.
+          </p>
+        </div>
+      </div>
+
+      <div class="mt-20 text-center">
+        <a
+          href="#download"
+          class="inline-block px-8 py-4 text-lg font-semibold rounded-lg bg-gradient-to-r from-purple-600 to-pink-600 text-white hover:from-purple-700 hover:to-pink-700 transform hover:-translate-y-1 transition-all duration-300"
+        >
+          Rejoindre l'Aventure
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
